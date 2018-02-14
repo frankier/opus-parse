eb872106591ee477cc60873835b0a12c6c527f8a
Frankie Robertson <frankie@robertson.name>
2018-02-14 19:01:29 +0200

    Add

---
+++ src/opensubtitles.rs
@@ -0,0 +1,35 @@
+fn entry_is_subtitle(entry: &DirEntry) -> bool {
+    entry.file_type().is_file() &&
+        entry.file_name().to_str().map(|s| s.ends_with(".xml.gz")).unwrap_or(false)
+}
+
+fn id_of_path(path: &Path) -> Option<u64> {
+    let mut components = path.components();
+    components.next_back();
+    components.next_back().and_then(|comp|
+        match comp {
+            Component::Normal(path) => Some(path),
+            _ => None
+        })
+        .and_then(|path| path.to_str())
+        .and_then(|path| path.parse::<u64>().ok())
+}
+
+/// Walk the tree of all subtitles for single language
+pub fn walk(path: &Path) -> Iterator<(u64, String)> {
+    let walker = WalkDir::new(collection_dir).into_iter();
+    let mut seen = HashSet::new();
+    walker
+        .filter_map(|e| e.ok()
+        .filter(entry_is_subtitle))
+        .filter_map(|subtitle_entry| {
+            let subtitle_path = subtitle_entry.path();
+            let movie_id = id_of_path(subtitle_path).unwrap();
+            if seen.contains(&movie_id) {
+                None
+            } else {
+                seen.insert(movie_id);
+                Some((movie_id, subtitle_path.to_owned()))
+            }
+        })
+}
